31e27aba8d37d075c6b8bd2a50a2e47c59914926
tharak1 <116187452+tharak1@users.noreply.github.com>
2023-11-19 15:06:56 +0530

    Update databaseConnection.js

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async()=>{
-        const connection = mongoose.connect(process.env.MONGO_KEY)
+        const connection = mongoose.connect("mongodb+srv://saitharakreddyv59:tharak@tharak.k1lxca6.mongodb.net/prism)
         .then(() => console.log("database connected"))
         .catch((err)=>console.log(err));
 }
